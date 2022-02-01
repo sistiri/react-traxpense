9187b9cc60ef73aacc7e581fbb1260bf15ca7382
sistiri <sistiri@yahoo.com>
2022-02-01 12:28:41 +0100

    Good Practice for Using One State w (prevState)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,63 +1,93 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('')
-    // const [enteredAmount, setEnteredAmount] = useState('')
-    // const [enteredDate, setEnteredDate] = useState('')
+  // const [enteredTitle, setEnteredTitle] = useState('')
+  // const [enteredAmount, setEnteredAmount] = useState('')
+  // const [enteredDate, setEnteredDate] = useState('')
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: '',
-    })
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
-    const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
-        })
-        // setEnteredTitle(event.target.value);
-        console.log(event.target.value);
-    };
+  const titleChangeHandler = (event) => {
+
+/* GOOD PRACTICE Using One State:  */
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+
+/* BAD PRACTICE Using One State: */
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle: event.target.value
+    // })
+
+/* MULTIPLE STATES: */
+    // setEnteredTitle(event.target.value);
     
-    const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
-        })
-        // setEnteredAmount(event.target.value);
-        console.log(event.target.value);
-    };
-    
-    const dateChangeHandler = (event) =>{
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
-        })
-        // setEnteredDate(event.target.value)
-        console.log(event.target.value);
-    }
+    console.log(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    });
+
+    // setUserInput({
+    //     ...userInput,
+    //     enteredAmount: event.target.value
+    // })
+
+    // setEnteredAmount(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
+
+    // setUserInput({
+    //     ...userInput,
+    //     enteredDate: event.target.value
+    // })
+
+    // setEnteredDate(event.target.value)
+    console.log(event.target.value);
+  };
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler} min="0.01" step="0.01"/>
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31"/>
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            min="2019-01-01"
+            max="2022-12-31"
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
