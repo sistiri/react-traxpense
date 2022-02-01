27e9b0bc566d9c28211760953acb6bb622e63b23
sistiri <sistiri@yahoo.com>
2022-02-01 16:00:14 +0100

    Rendering Lists of Data,  Stateful Lists & Keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,7 +17,17 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
+        {props.items.map((expense) => (
+          <ExpenseItem 
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+
+        ) 
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -36,7 +46,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
