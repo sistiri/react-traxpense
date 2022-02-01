f433269a75b6ba22acda1cbbea58d78adeb4bb22
sistiri <sistiri@yahoo.com>
2022-02-01 14:30:31 +0100

    Set ExpenseItem Component back to Stateless

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,21 +6,15 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title)
-
-  const clickHandler = () => {
-    setTitle('Updated!!');
-    console.log(title)
-  }
+console.log('ExpenseItem evaluated by React')
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
