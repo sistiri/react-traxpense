6bd77074fdfed85bcc79123deefa32d8c075b3d8
sistiri <sistiri@yahoo.com>
2022-02-02 15:55:38 +0100

    Adding Dynamic Styles to Chart Bars

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -24,6 +26,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
