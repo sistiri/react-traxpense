0670ddd28761c38acb0a8c24c74abe1aa62783d7
sistiri <sistiri@yahoo.com>
2022-02-02 11:34:36 +0100

    Conditional Content with AND operator

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -24,14 +24,16 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.length === 0 ? <p>No Expenses found.</p> : (filteredExpenses.map((expense) => (
+        {filteredExpenses.length === 0 &&  <p>No Expenses found.</p>}
+        {filteredExpenses.length > 0 && 
+        filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
-        )))}
+        ))}
         )
 
       </Card>
